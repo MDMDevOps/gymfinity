8c6f135d4c9eb64943eb9c0728f0ce8a08f9caba
Bob Moore <bob.moore@midwestdigitalmarketing.com>
2017-04-18 11:30:58 -0500

    Changed up the sidebar navigation

--- themes/gymfinity/scripts/public.js
+++ themes/gymfinity/scripts/public.js
@@ -4,90 +4,95 @@ if( $ ) { $.noConflict(); }
 jQuery( function( $ ) {
 	'use strict';
 
-	var $fs = $.map( $( '.fixed-scroll' ), function( element ) {
-		return new FixedScrollingSidebar( element );
-	});
+	/*
+	 * Fixed sidebar Module
+	 * May use later, but being deprecated for now until a better solution can be thought of.
+	 */
 
-	function FixedScrollingSidebar( sidebar ) {
-		var $sidebar, $column, $container, settings = {};
+		// var $fs = $.map( $( '.fixed-scroll' ), function( element ) {
+		// 	return new FixedScrollingSidebar( element );
+		// });
 
-		var cacheDom = function() {
-			$sidebar   = $( sidebar );
-			$column    = $sidebar.find( '.fixed-scroll-column' ).first();
-			$container = $( $sidebar.data( 'container' ) );
-		};
+		// function FixedScrollingSidebar( sidebar ) {
+		// 	var $sidebar, $column, $container, settings = {};
 
-		var calculate = function() {
-			settings = {
-				'width'  : $sidebar.width(),
-				'top'    : $sidebar.data( 'offset' ) || 0,
-				'height' : $column.height(),
-			};
-			// console.log( $container.height() );
-			$(window).on('scroll', function(){
-				// console.log( $(document).scrollTop() );
-			});
-		};
+		// 	var cacheDom = function() {
+		// 		$sidebar   = $( sidebar );
+		// 		$column    = $sidebar.find( '.fixed-scroll-column' ).first();
+		// 		$container = $( $sidebar.data( 'container' ) );
+		// 	};
 
-		var bindEvents = function() {
-			$sidebar.waypoint( scrollTrigger, { offset : settings.top } );
-			// var newoff = 0 - ( $container.height() - $sidebar.height() - parseInt( settings.top ));
-			$container.waypoint( stopScroll, { offset : calculateStop } );
-			// Resizer column width
-			$( window ).on( 'resize', resizeWidth );
-		};
+		// 	var calculate = function() {
+		// 		settings = {
+		// 			'width'  : $sidebar.width(),
+		// 			'top'    : $sidebar.data( 'offset' ) || 0,
+		// 			'height' : $column.height(),
+		// 		};
+		// 		// console.log( $container.height() );
+		// 		$(window).on('scroll', function(){
+		// 			// console.log( $(document).scrollTop() );
+		// 		});
+		// 	};
 
-		var calculateStop = function() {
-			console.log( $column.height() );
-			return 0 - ( $container.height() - $column.height() - parseInt( settings.top ) );
-		};
+		// 	var bindEvents = function() {
+		// 		$sidebar.waypoint( scrollTrigger, { offset : settings.top } );
+		// 		// var newoff = 0 - ( $container.height() - $sidebar.height() - parseInt( settings.top ));
+		// 		$container.waypoint( stopScroll, { offset : calculateStop } );
+		// 		// Resizer column width
+		// 		$( window ).on( 'resize', resizeWidth );
+		// 	};
 
-		var setWidth = function() {
-			// calculate();
-			$column.css( { 'width' : $sidebar.width() + 'px' } );
-		};
+		// 	var calculateStop = function() {
+		// 		console.log( $column.height() );
+		// 		return 0 - ( $container.height() - $column.height() - parseInt( settings.top ) );
+		// 	};
 
-		var scrollTrigger = function( direction ) {
-			if( direction === 'down' ) {
-				// $( window ).on( 'scroll', scrollEffect );
-				$column.css( { 'width' : $sidebar.width() + 'px', 'position' : 'fixed', 'top' : settings.top } );
+		// 	var setWidth = function() {
+		// 		// calculate();
+		// 		$column.css( { 'width' : $sidebar.width() + 'px' } );
+		// 	};
 
-			} else {
-				console.log( direction );
-				// $( window ).off( 'scroll', scrollEffect );
-				$column.css( { 'position' : 'relative', 'top' : 0  } );
-			}
+		// 	var scrollTrigger = function( direction ) {
+		// 		if( direction === 'down' ) {
+		// 			// $( window ).on( 'scroll', scrollEffect );
+		// 			$column.css( { 'width' : $sidebar.width() + 'px', 'position' : 'fixed', 'top' : settings.top } );
 
-			return;
-		};
+		// 		} else {
+		// 			console.log( direction );
+		// 			// $( window ).off( 'scroll', scrollEffect );
+		// 			$column.css( { 'position' : 'relative', 'top' : 0  } );
+		// 		}
 
-		var resizeWidth = function() {
-			$column.css( { 'width' : $sidebar.width() + 'px' } );
-		};
+		// 		return;
+		// 	};
 
-		var stopScroll = function( direction ) {
+		// 	var resizeWidth = function() {
+		// 		$column.css( { 'width' : $sidebar.width() + 'px' } );
+		// 	};
 
-			if( direction === 'down' ) {
-				// $sidebar.css( { 'position' : 'relative', 'height' : $container.height() } );
-				$column.css( { 'position' : 'absolute', 'top' : $container.height() - $column.height() } );
-			} else {
-				$column.css( { 'width' : $sidebar.width() + 'px', 'position' : 'fixed', 'top' : settings.top } );
-				// $sidebar.css( { 'position' : 'relative', 'height' : 'auto' } ).promise().done( function() {
-				// 	$column.attr( 'style', '' );
-					// $column.css( { 'top' : '50px', background : 'blue'} );
-				// });
+		// 	var stopScroll = function( direction ) {
 
-			}
-		};
+		// 		if( direction === 'down' ) {
+		// 			// $sidebar.css( { 'position' : 'relative', 'height' : $container.height() } );
+		// 			$column.css( { 'position' : 'absolute', 'top' : $container.height() - $column.height() } );
+		// 		} else {
+		// 			$column.css( { 'width' : $sidebar.width() + 'px', 'position' : 'fixed', 'top' : settings.top } );
+		// 			// $sidebar.css( { 'position' : 'relative', 'height' : 'auto' } ).promise().done( function() {
+		// 			// 	$column.attr( 'style', '' );
+		// 				// $column.css( { 'top' : '50px', background : 'blue'} );
+		// 			// });
 
-		( function() {
-			cacheDom();
-			calculate();
-			bindEvents();
-		})();
+		// 		}
+		// 	};
 
-		return $sidebar;
-	}
+		// 	( function() {
+		// 		cacheDom();
+		// 		calculate();
+		// 		bindEvents();
+		// 	})();
+
+		// 	return $sidebar;
+		// }
 
 	// $('.sortable-table').DataTable();
 });
