aee9637cd5bf898605abb794f9d7a0cc5b94b733
Bob Moore <bob.moore@midwestdigitalmarketing.com>
2017-04-20 09:00:26 -0500

    Added event view

--- themes/gymfinity/views/excerpt.php
+++ themes/gymfinity/views/excerpt.php
@@ -7,15 +7,21 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'excerpt' ); ?> itemscope itemtype="http://schema.org/BlogPosting">
+	<div class="excerpt-meta">
+		<?php do_action( 'mpress_entry_meta' ); ?>
+	</div>
 	<header class="excerpt-header">
 		<?php do_action( 'mpress_entry_title', 'excerpt-title' ); ?>
-		<div class="excerpt-meta">
-			<?php do_action( 'mpress_entry_meta' ); ?>
-		</div>
 	</header>
-	<div class="excerpt-content" itemprop="text articleBody">
-		<?php the_post_thumbnail(); ?>
-		<?php the_excerpt(); ?>
+	<div class="excerpt-content-wrapper" itemprop="text articleBody">
+		<?php if( has_post_thumbnail() ) : ?>
+			<div class="excerpt-thumbnail">
+				<?php the_post_thumbnail( 'gymfinity-featured-small' ); ?>
+			</div>
+		<?php endif; ?>
+		<div class="excerpt-content">
+			<?php the_excerpt(); ?>
+		</div>
 	</div>
 	<footer class="excerpt-footer">
 		<?php do_action( 'jp_share_buttons' ); ?>
