f706925861256ebc1ede680000b07707534b98a9
Mike <mallen1189@gmail.com>
2017-05-31 18:01:22 -0500

    update app bar

--- themes/gymfinity/partials/banner-appbar.php
+++ themes/gymfinity/partials/banner-appbar.php
@@ -1,3 +1,5 @@
 <div id="appbar">
-	<button class="menu-toggle" aria-expanded="false" data-toggle="<?php echo get_theme_mod( 'mpress_menu_type' ); ?>"><?php do_action( 'mpress_icon', 'menu' ) ?><span class="screen-reader-text"><?php esc_html_e( 'Menu', 'mpress' ); ?></span></button>
+	<a class="menu-toggle appbar-icons" aria-expanded="false" data-toggle="<?php echo get_theme_mod( 'mpress_menu_type' ); ?>"><?php do_action( 'mpress_icon', 'menu' ) ?><span class="screen-reader-text"><?php esc_html_e( 'Menu', 'mpress' ); ?></span></a>
+	<a class="appbar-icons" href="tel:6088483547"><span class="fa fa-phone"></span></a>
+	<a class="appbar-icons" href="https://www.google.com/maps/place/Gymfinity/@43.0086102,-89.4853496,17z/data=!3m1!4b1!4m5!3m4!1s0x8807b1f01b7adfef:0x7e8f4579fb0ef45e!8m2!3d43.0086102!4d-89.4831609"><span class="fa fa fa-map-marker"></span></a>
 </div>
